871a20bd80547292ba80cb23f76a768a4dfd42ef
Kira Hartlage <kira.hartlage@gmail.com>
2017-05-30 18:58:07 -0400

    completed challenge 4

---
+++ js/form.js
@@ -0,0 +1,34 @@
+// Use jQuery's keyup method to check if an email is valid after
+// each key stroke. Show a validation message below the input field
+// and disable the submit button if the email is not valid.
+$(document).ready(function() {
+
+  $('input[type=submit]').prop('disabled', true);
+  $('#validation').hide();
+
+  function isValidEmailAddress(emailAddress) {
+      var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+      return pattern.test(emailAddress);
+  };
+
+  $('#emailField').focus(function() {
+    $('#validation').show();
+    $('#emailField').keyup(function() {
+      let emailAddress = $('#emailField').val();
+      if (isValidEmailAddress(emailAddress)) {
+        console.log("good address");
+        $('#validation').hide();
+        $('input[type=submit]').prop('disabled', false);
+      }
+      else {
+        $('#validation').show();
+      }
+    });
+  })
+
+  $('input[type=submit]').click(function() {
+    console.log('you clicked the button');
+    alert("Server unavailable, try again later");
+    $('#emailField').val('');
+  });
+});
